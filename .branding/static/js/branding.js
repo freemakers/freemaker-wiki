b00036cfafec4f0606ec37196a2a0b30c4d8800d
John Casey <jdcasey@commonjava.org>
2013-09-27 23:50:16 -0500

    Add license notices to the footer.

--- .branding/static/js/branding.js
+++ .branding/static/js/branding.js
@@ -3,6 +3,6 @@ $('.branding-header').html(
   '<img id="fm-logo" height="125px" src="/static/images/FreeMakers-Logo1.png"/><div id="fm-title"><span id="fm-title-content">Free State Maker Society</span></div>'
 );
 
-// $('footer').prepend(
-//   '<div id="jhcaccounting-footer">Questions? Problems? Email: <a href="mailto:jdcasey@commonjava.org?subject=Wiki">John</a></div>'
-// );
+$('footer').prepend(
+  '<div id="fm-footer"><div class="license"><a href="/static/gplv3.txt"><img src="/static/images/gplv3.png"/></a><br/>Software</div><div class="license"><a href="/static/cc-by-sa.txt"><img src="/static/images/cc-by-sa.png"/></a><br/>Everything Else</div></div>'
+);
