b9b1752f6e7005f52f7e02f1030d4aa1340a4c36
John Casey <jdcasey@commonjava.org>
2013-09-27 22:24:23 -0500

    Starting to work on the branding for the wiki.

---
+++ .branding/static/js/branding.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+$('.branding-header').html(
+  '<img id="fm-logo" height="125px" src="/static/images/FreeMakers-Logo1.png"/><div id="fm-title"><span id="fm-title-content">Free State Maker Society</span></div>'
+);
+
+// $('footer').prepend(
+//   '<div id="jhcaccounting-footer">Questions? Problems? Email: <a href="mailto:jdcasey@commonjava.org?subject=Wiki">John</a></div>'
+// );
